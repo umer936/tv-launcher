2e032bb436569a5a7240ef33be7364c0a971efca
Umer Salman <umer936@gmail.com>
2025-11-01 13:25:03 -0500

    Improve icons

--- web/main.js
+++ web/main.js
@@ -6,9 +6,11 @@ new QWebChannel(qt.webChannelTransport, function(channel) {
         .then(apps => {
             const grid = document.getElementById('grid');
             apps.forEach(a => {
+                // Convert app name to safe filename
+                const iconName = a.name.replace(/ /g, "_") + ".png";
                 const tile = document.createElement('div');
                 tile.className = 'tile';
-                tile.innerHTML = `<img src="../icons/${a.icon.split('/').pop()}"><span>${a.name}</span>`;
+                tile.innerHTML = `<img src="../icons/${iconName}"><span>${a.name}</span>`;
                 tile.onclick = () => backend.launch(a.name);
                 grid.appendChild(tile);
             });
